973ad34fab4d0e39edf24bbcd19b8a95a40bb54d
petya0927 <unyipeti@gmail.com>
2020-06-19 17:03:41 +0200

    Face detection on video

---
+++ cv_face_detection_live.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture("video.mp4")
+cap.set(3, 640)
+cap.set(4, 480)
+
+faceCascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+
+while True:
+    ret, frame = cap.read()
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(grayFrame, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Video", frame)
+    
+    if cv2.waitKey(1) == 27:
+        break
