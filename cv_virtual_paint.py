435c28fc548181dcb03c58d37a35740c31ab3ce8
petya0927 <unyipeti@gmail.com>
2020-06-21 13:15:47 +0200

    Javítások + Virtual paint

--- cv_virtual_paint.py
+++ cv_virtual_paint.py
@@ -1,22 +1,66 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 cap.set(10, 100)
 
-def findColor(img):
+
+myColors = [[156, 135, 139, 179, 255, 255],
+            [0, 153, 175, 21, 255, 255],
+            [74, 220, 22, 95, 255, 255]]
+
+
+myColorValues = [[178, 102, 255],
+                 [51, 153, 255],
+                 [0, 255, 0]]
+
+myPoints = []                         #[x, y, colorID]
+
+def findColor(img, myColors, myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-    mask = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow("img", mask)
+    count = 0
+    newPoints = []
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        x, y = getContours(mask)
+        cv2.circle(imgResult, (x, y), 10, (myColorValues[count]), cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x, y, count])
+        count += 1
+        #cv2.imshow(str(color[0]), mask)
+    return newPoints
+        
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)   
+    return (x+w//2, y)
 
-
+def drawOnCanvas(myPoints, myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult, (point[0], point[1]), 10, (myColorValues[point [2]]), cv2.FILLED)
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Video", img)
+    imgResult = img.copy()
+    newPoints = findColor(img, myColors, myColorValues)
+    if len(newPoints) != 0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints) != 0:
+        drawOnCanvas(myPoints, myColorValues)
+    cv2.imshow("Video", imgResult)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
 
